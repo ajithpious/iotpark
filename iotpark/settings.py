f4dc5398f8d6c7e2c386c19052e68caf317a92fe
Ajith pious <ajithpiousolive@gmail.com>
2022-04-04 15:40:27 +0100

    final changes

--- iotpark/settings.py
+++ iotpark/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku settings
+import django_heroku
+django_heroku.settings(locals())
